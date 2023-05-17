b3fb092acd547b0c593f7ca66bbd586067856294
JeraldInfant01 <jeraldinfant01@gmail.com>
2023-05-17 19:37:42 +0530

    codepushed

---
+++ src/If/els/Age.java
@@ -0,0 +1,21 @@
+package If.els;
+
+import java.util.Iterator;
+
+public class Age {
+public static void main(String[] args) {
+	
+	int age=18;
+	if(age<18) {
+		System.out.println("School ");
+	}else
+	{
+		System.out.println("college");
+	}
+	
+
+}
+	
+	
+	
+}
